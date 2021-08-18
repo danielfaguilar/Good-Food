e141ff0c00d5ed870293915036fd9f2c6a44966b
Daniel Flores <daniel.flores.fullstack@gmail.com>
2021-08-18 14:45:56 -0500

    Creation of the MainActivity for navigate between fragments

---
+++ app/src/main/java/com/favorezapp/goodfood/common/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.favorezapp.goodfood.common
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.favorezapp.goodfood.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
