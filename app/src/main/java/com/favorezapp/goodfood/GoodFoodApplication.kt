3686900b57194cccaa78ead5ec1fe5008649bc85
Daniel Flores <daniel.flores.fullstack@gmail.com>
2021-08-18 14:37:08 -0500

    Creating new application component for setup the dependency graph with hilt and for setup the logging module

--- app/src/main/java/com/favorezapp/goodfood/GoodFoodApplication.kt
+++ app/src/main/java/com/favorezapp/goodfood/GoodFoodApplication.kt
@@ -1,4 +1,16 @@
 package com.favorezapp.goodfood
 
-class GoodFoodApplication {
+import android.app.Application
+import com.favorezapp.logging.Logger
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class GoodFoodApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        initLogger()
+    }
+    private fun initLogger() {
+        Logger.init()
+    }
 }
