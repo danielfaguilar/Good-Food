ba889f15703044acbb53e568590f16cca2d5d40a
Daniel Flores <daniel.flores.fullstack@gmail.com>
2021-08-18 14:38:07 -0500

    Adding interceptors for network state and logging

--- app/src/main/java/com/favorezapp/goodfood/common/data/api/interceptors/LoggingInterceptor.kt
+++ app/src/main/java/com/favorezapp/goodfood/common/data/api/interceptors/LoggingInterceptor.kt
@@ -1,4 +1,11 @@
 package com.favorezapp.goodfood.common.data.api.interceptors
 
-class LoggingInterceptor {
+import com.favorezapp.logging.Logger
+import okhttp3.logging.HttpLoggingInterceptor
+import javax.inject.Inject
+
+class LoggingInterceptor @Inject constructor(): HttpLoggingInterceptor.Logger {
+    override fun log(message: String) {
+        Logger.i(message)
+    }
 }
--- app/src/main/java/com/favorezapp/goodfood/common/data/api/interceptors/NetworkStatusInterceptor.kt
+++ app/src/main/java/com/favorezapp/goodfood/common/data/api/interceptors/NetworkStatusInterceptor.kt
@@ -1,4 +1,16 @@
 package com.favorezapp.goodfood.common.data.api.interceptors
 
-class NetworkStatusInterceptor {
+import com.favorezapp.goodfood.common.data.api.util.ConnectionManager
+import com.favorezapp.goodfood.common.domain.model.NetworkUnavailableException
+import okhttp3.Interceptor
+import javax.inject.Inject
+
+class NetworkStatusInterceptor @Inject constructor(
+    private val connectionManager: ConnectionManager
+): Interceptor {
+    override fun intercept(chain: Interceptor.Chain) =
+        if( connectionManager.isConnected )
+            chain.proceed(chain.request())
+        else
+            throw NetworkUnavailableException()
 }
