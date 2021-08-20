8d6fac2b24620d8eafbbca61c4c6d3f006092866
Daniel Flores <daniel.flores.fullstack@gmail.com>
2021-08-19 22:02:12 -0500

    Working on the favoritesrecipes feature

---
+++ app/src/main/java/com/favorezapp/goodfood/favoritesrecipes/FavoritesFoodRecipesFragment.kt
@@ -0,0 +1,7 @@
+package com.favorezapp.goodfood.favoritesrecipes
+
+import androidx.fragment.app.Fragment
+import com.favorezapp.goodfood.R
+
+class FavoritesFoodRecipesFragment :
+    Fragment(R.layout.fragment_favorites_food_recipes)
