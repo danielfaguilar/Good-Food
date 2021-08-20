1fa610f3b96cfb76d6038aac52e7a30fe49dded4
Daniel Flores <daniel.flores.fullstack@gmail.com>
2021-08-19 22:06:55 -0500

    Setup of the navigation in the MainActivity and his layout

--- app/src/main/java/com/favorezapp/goodfood/common/MainActivity.kt
+++ app/src/main/java/com/favorezapp/goodfood/common/MainActivity.kt
@@ -2,11 +2,30 @@ package com.favorezapp.goodfood.common
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.favorezapp.goodfood.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navController = findNavController(R.id.nav_host_fragment_container)
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation_view)
+        bottomNavigationView.setupWithNavController(navController)
+
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.allFoodRecipesFragment,
+                R.id.favoritesFoodRecipesFragment,
+                R.id.foodJokeFragment
+            )
+        )
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
     }
 }
