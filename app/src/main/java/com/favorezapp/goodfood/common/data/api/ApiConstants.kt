1648672a3ab889c61465babdd84dc9b299cb6bad
Daniel Flores <daniel.flores.fullstack@gmail.com>
2021-08-20 22:13:47 -0500

    Working on the api of the data layer

--- app/src/main/java/com/favorezapp/goodfood/common/data/api/ApiConstants.kt
+++ app/src/main/java/com/favorezapp/goodfood/common/data/api/ApiConstants.kt
@@ -1,6 +1,14 @@
 package com.favorezapp.goodfood.common.data.api
 
+import com.favorezapp.goodfood.BuildConfig
+
 object ApiConstants {
     const val BASE_ENDPOINT = "https://api.spoonacular.com/"
     const val RECIPES_ENDPOINT = "recipes/complexSearch"
+
+    const val API_KEY = BuildConfig.Key
+}
+
+object ApiParameters {
+    const val KEY_QUERY = "apiKey"
 }
--- app/src/main/java/com/favorezapp/goodfood/common/data/api/interceptors/AuthenticationInterceptor.kt
+++ app/src/main/java/com/favorezapp/goodfood/common/data/api/interceptors/AuthenticationInterceptor.kt
@@ -1,4 +1,24 @@
 package com.favorezapp.goodfood.common.data.api.interceptors
 
-class AuthenticationInterceptor {
+import com.favorezapp.goodfood.common.data.api.ApiConstants
+import com.favorezapp.goodfood.common.data.api.ApiParameters
+import okhttp3.Interceptor
+import okhttp3.Response
+import javax.inject.Inject
+
+class AuthenticationInterceptor @Inject constructor(): Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val originalRequest = chain.request()
+        val originalHttpUrl = originalRequest.url
+
+        val httpUrl = originalHttpUrl.newBuilder()
+            .addQueryParameter(ApiParameters.KEY_QUERY, ApiConstants.API_KEY)
+            .build()
+
+        val requestBuilder = originalRequest.newBuilder()
+            .url(httpUrl)
+
+        val authRequest = requestBuilder.build()
+        return chain.proceed(authRequest)
+    }
 }
