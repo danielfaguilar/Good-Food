dbc8d53bdb64dd7d531a00a4483ef3af7e50b7b7
Daniel Flores <daniel.flores.fullstack@gmail.com>
2021-08-28 11:17:40 -0500

    Adding use cases and methods for subscribing to the view state updates for the allrecipes feature

--- app/src/main/java/com/favorezapp/goodfood/allrecipes/presentation/AllFoodRecipesFragmentViewModel.kt
+++ app/src/main/java/com/favorezapp/goodfood/allrecipes/presentation/AllFoodRecipesFragmentViewModel.kt
@@ -4,13 +4,19 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.favorezapp.goodfood.allrecipes.domain.usecases.GetRecipes
+import com.favorezapp.goodfood.allrecipes.domain.usecases.RequestNextFoodRecipes
 import com.favorezapp.goodfood.common.domain.model.NetworkException
 import com.favorezapp.goodfood.common.domain.model.NetworkUnavailableException
 import com.favorezapp.goodfood.common.domain.model.NoMoreRecipesException
+import com.favorezapp.goodfood.common.domain.model.foodrecipe.FoodRecipe
+import com.favorezapp.goodfood.common.domain.model.pagination.Pagination
 import com.favorezapp.goodfood.common.presentation.Event
+import com.favorezapp.goodfood.common.presentation.model.mappers.UIFoodRecipeMapper
 import com.favorezapp.goodfood.common.utils.DispatchersProvider
 import com.favorezapp.goodfood.common.utils.createExceptionHandler
 import dagger.hilt.android.lifecycle.HiltViewModel
+import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.launch
@@ -19,8 +25,11 @@ import javax.inject.Inject
 
 @HiltViewModel
 class AllFoodRecipesFragmentViewModel @Inject constructor(
-    private val compositeDisposable: CompositeDisposable,
-    private val dispatchersProvider: DispatchersProvider
+    private val requestNextFoodRecipes: RequestNextFoodRecipes,
+    private val getRecipes: GetRecipes,
+    private val uiFoodRecipeMapper: UIFoodRecipeMapper,
+    private val dispatchersProvider: DispatchersProvider,
+    private val compositeDisposable: CompositeDisposable
 ): ViewModel() {
     private val _state = MutableLiveData<AllFoodRecipesFragmentViewState>()
     val state: LiveData<AllFoodRecipesFragmentViewState>
@@ -30,6 +39,7 @@ class AllFoodRecipesFragmentViewModel @Inject constructor(
 
     init {
         _state.value = AllFoodRecipesFragmentViewState()
+        subscribeToRecipesUpdates()
     }
 
     fun onEvent(event: AllFoodRecipesEvent) =
@@ -37,6 +47,29 @@ class AllFoodRecipesFragmentViewModel @Inject constructor(
             is AllFoodRecipesEvent.RequestInitialRecipes -> { loadRecipes() }
             is AllFoodRecipesEvent.RequestMoreRecipes -> {}
         }
+
+    private fun subscribeToRecipesUpdates() {
+        val disposable = getRecipes()
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(
+                { onNewRecipesList(it) },
+                { onFailure(it) }
+            )
+
+        compositeDisposable.add(disposable)
+    }
+
+    private fun onNewRecipesList(it: List<FoodRecipe>) {
+        val recipes = it.map {
+            uiFoodRecipeMapper.mapToView(it)
+        }
+
+        val currentList = state.value!!.recipes
+        val newItems = recipes.subtract(currentList)
+        val updatedList = currentList + newItems
+
+        _state.value = state.value!!.copy( loading = false, recipes = updatedList )
+    }
 
     private fun loadRecipes() {
         if( state.value!!.recipes.isEmpty() )
@@ -47,8 +80,16 @@ class AllFoodRecipesFragmentViewModel @Inject constructor(
         val exceptionHandler = createExceptionHandler("Error loading recipes")
 
         viewModelScope.launch(exceptionHandler) {
-            // Callt the use case
+            val pagination = withContext(dispatchersProvider.io()) {
+                requestNextFoodRecipes()
+            }
+
+            onPaginationInfoObtained(pagination)
         }
+    }
+
+    private fun onPaginationInfoObtained(pagination: Pagination) {
+        numberOfRecipes = pagination.numOfRecipes
     }
 
     private fun createExceptionHandler(message: String): CoroutineExceptionHandler =
