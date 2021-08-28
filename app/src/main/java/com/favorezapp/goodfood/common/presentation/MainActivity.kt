968429681570a72203b11afa2cbef9ee17885a80
Daniel Flores <daniel.flores.fullstack@gmail.com>
2021-08-28 11:20:54 -0500

    Unused imports and unnecessary comments deleted

--- app/src/main/java/com/favorezapp/goodfood/common/presentation/MainActivity.kt
+++ app/src/main/java/com/favorezapp/goodfood/common/presentation/MainActivity.kt
@@ -1,21 +1,29 @@
-package com.favorezapp.goodfood.common
+package com.favorezapp.goodfood.common.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.favorezapp.goodfood.R
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.favorezapp.goodfood.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val navController = findNavController(R.id.nav_host_fragment_container)
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation_view)
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        val bottomNavigationView = binding.bottomNavigationView
         bottomNavigationView.setupWithNavController(navController)
 
         val appBarConfiguration = AppBarConfiguration(
