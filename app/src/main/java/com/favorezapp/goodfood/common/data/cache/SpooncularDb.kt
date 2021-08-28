d2293a3ddcdb75d74cb5989c675dcbefde853cf3
Daniel Flores <daniel.flores.fullstack@gmail.com>
2021-08-28 11:29:43 -0500

    Adding the database

--- app/src/main/java/com/favorezapp/goodfood/common/data/cache/SpooncularDb.kt
+++ app/src/main/java/com/favorezapp/goodfood/common/data/cache/SpooncularDb.kt
@@ -1,3 +1,23 @@
-package com.favorezapp.goodfood.common.data.cache.foodrecipe
+package com.favorezapp.goodfood.common.data.cache
 
-class SpooncularDb
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.favorezapp.goodfood.common.data.cache.daos.FoodRecipeDao
+import com.favorezapp.goodfood.common.data.cache.models.foodrecipe.*
+
+const val DB_VERSION = 1
+const val DB_NAME = "spooncular_db"
+
+@Database(
+    entities = [
+        CachedCuisine::class,
+        CachedFoodRecipe::class,
+        CachedExtendedIngredient::class,
+        CachedRecipeIngredientCrossRef::class,
+        CachedFoodRecipeCuisineCrossRef::class
+    ],
+    version = DB_VERSION
+)
+abstract class SpooncularDb: RoomDatabase() {
+    abstract fun foodRecipeDao(): FoodRecipeDao
+}
