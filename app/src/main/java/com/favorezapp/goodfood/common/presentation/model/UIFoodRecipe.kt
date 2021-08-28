f38520c81f80ac54a4b1931f86feee381d36a51e
Daniel Flores <daniel.flores.fullstack@gmail.com>
2021-08-28 11:33:44 -0500

    FoodRecipes model for the presentation layer

--- app/src/main/java/com/favorezapp/goodfood/common/presentation/model/UIFoodRecipe.kt
+++ app/src/main/java/com/favorezapp/goodfood/common/presentation/model/UIFoodRecipe.kt
@@ -3,8 +3,8 @@ package com.favorezapp.goodfood.common.presentation.model
 data class UIFoodRecipe(
     val id: Long,
     val image: String,
-    val name: String,
-    val description: String,
+    val title: String,
+    val summary: String,
     val likes: Int,
     val readyInMinutes: Int,
     val vegan: Boolean
