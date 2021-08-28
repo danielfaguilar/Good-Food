993b16cefd50135e3d313a2d6400da92b26d8f84
Daniel Flores <daniel.flores.fullstack@gmail.com>
2021-08-28 11:18:39 -0500

    Changing initial view state loading variable to true, because initial is supposed to be loading data

--- app/src/main/java/com/favorezapp/goodfood/allrecipes/presentation/AllFoodRecipesFragmentViewState.kt
+++ app/src/main/java/com/favorezapp/goodfood/allrecipes/presentation/AllFoodRecipesFragmentViewState.kt
@@ -4,7 +4,7 @@ import com.favorezapp.goodfood.common.presentation.Event
 import com.favorezapp.goodfood.common.presentation.model.UIFoodRecipe
 
 data class AllFoodRecipesFragmentViewState(
-    val loading: Boolean = false,
+    val loading: Boolean = true,
     val recipes: List<UIFoodRecipe> = emptyList(),
     val noMoreRecipes: Boolean = false,
     val failure: Event<Throwable>? = null
