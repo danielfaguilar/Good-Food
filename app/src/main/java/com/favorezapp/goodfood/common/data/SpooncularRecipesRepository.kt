5f9b77306d2d5be2eb92718b116e1ad2307187f2
Daniel Flores <daniel.flores.fullstack@gmail.com>
2021-08-28 11:22:41 -0500

    Cached capabilities added to the repository

--- app/src/main/java/com/favorezapp/goodfood/common/data/SpooncularRecipesRepository.kt
+++ app/src/main/java/com/favorezapp/goodfood/common/data/SpooncularRecipesRepository.kt
@@ -3,14 +3,19 @@ package com.favorezapp.goodfood.common.data
 import com.favorezapp.goodfood.common.data.api.SpooncularApi
 import com.favorezapp.goodfood.common.data.api.model.mappers.ApiFoodRecipeMapper
 import com.favorezapp.goodfood.common.data.api.model.mappers.ApiPaginatedFoodRecipesMapper
+import com.favorezapp.goodfood.common.data.cache.Cache
+import com.favorezapp.goodfood.common.data.cache.models.foodrecipe.CachedFoodRecipeAggregate
 import com.favorezapp.goodfood.common.domain.RecipesRepository
 import com.favorezapp.goodfood.common.domain.model.NoMoreRecipesException
+import com.favorezapp.goodfood.common.domain.model.foodrecipe.FoodRecipe
 import com.favorezapp.goodfood.common.domain.model.pagination.PaginatedFoodRecipes
 import com.favorezapp.goodfood.common.domain.model.pagination.Pagination
+import io.reactivex.Flowable
 import javax.inject.Inject
 
 class SpooncularRecipesRepository @Inject constructor(
     private val api: SpooncularApi,
+    private val cache: Cache,
     private val apiFoodRecipeMapper: ApiFoodRecipeMapper,
     private val apiPaginationFoodRecipesMapper: ApiPaginatedFoodRecipesMapper
 ): RecipesRepository {
@@ -32,5 +37,24 @@ class SpooncularRecipesRepository @Inject constructor(
                 totalRecipes ?: 0
             )
         )
+    }
+
+    override suspend fun storeFoodRecipes(foodRecipes: List<FoodRecipe>) {
+        cache.storeFoodRecipes(
+            foodRecipes.map {
+                CachedFoodRecipeAggregate.fromDomain(it)
+            }
+        )
+    }
+
+    override fun getRecipes(): Flowable<List<FoodRecipe>> {
+        return cache
+            .getFoodRecipes()
+            .distinctUntilChanged()
+            .map { foodRecipeList ->
+                foodRecipeList.map {
+                    it.foodRecipe.toDomain(it.cuisines, it.extendedIngredients)
+                }
+            }
     }
 }
