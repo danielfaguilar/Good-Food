29ef3f47026af3e82973410bb42b01e093d7e527
Daniel Flores <daniel.flores.fullstack@gmail.com>
2021-08-28 11:32:18 -0500

    Provides repository for the dependency graph, uses retained activity scope because is used by the view model activity

--- app/src/main/java/com/favorezapp/goodfood/common/di/ActivityRetainedModule.kt
+++ app/src/main/java/com/favorezapp/goodfood/common/di/ActivityRetainedModule.kt
@@ -1,16 +1,24 @@
 package com.favorezapp.goodfood.common.di
 
+import com.favorezapp.goodfood.common.data.SpooncularRecipesRepository
+import com.favorezapp.goodfood.common.domain.RecipesRepository
 import com.favorezapp.goodfood.common.utils.CoroutineDispatchersProvider
 import com.favorezapp.goodfood.common.utils.DispatchersProvider
 import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ActivityRetainedComponent
 import io.reactivex.disposables.CompositeDisposable
 
 @Module
-@InstallIn(ActivityRetainedModule::class)
+@InstallIn(ActivityRetainedComponent::class)
 abstract class ActivityRetainedModule {
+
+    @Binds
+    @ActivityRetainedScope
+    abstract  fun bindsRepository( repo: SpooncularRecipesRepository ): RecipesRepository
+
     companion object {
         @Provides
         fun providesCompositeDisposable() = CompositeDisposable()
