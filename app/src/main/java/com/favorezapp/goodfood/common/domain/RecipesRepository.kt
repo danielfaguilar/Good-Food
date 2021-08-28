4695777a4f807d7cced94a5c18efd016dcba11b5
Daniel Flores <daniel.flores.fullstack@gmail.com>
2021-08-28 11:32:40 -0500

    Repository contract

--- app/src/main/java/com/favorezapp/goodfood/common/domain/RecipesRepository.kt
+++ app/src/main/java/com/favorezapp/goodfood/common/domain/RecipesRepository.kt
@@ -1,7 +1,11 @@
 package com.favorezapp.goodfood.common.domain
 
+import com.favorezapp.goodfood.common.domain.model.foodrecipe.FoodRecipe
 import com.favorezapp.goodfood.common.domain.model.pagination.PaginatedFoodRecipes
+import io.reactivex.Flowable
 
 interface RecipesRepository {
     suspend fun requestMoreFoodRecipes(): PaginatedFoodRecipes
+    suspend fun storeFoodRecipes(foodRecipes: List<FoodRecipe>)
+    fun getRecipes(): Flowable<List<FoodRecipe>>
 }
