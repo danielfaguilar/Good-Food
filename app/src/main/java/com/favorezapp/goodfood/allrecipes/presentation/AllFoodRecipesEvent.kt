19e6bb2e7bf14b805e4467c65baa26fee96a4317
Daniel Flores <daniel.flores.fullstack@gmail.com>
2021-09-09 22:10:22 -0500

    Working on the presentation layer for the allrecipes feature

--- app/src/main/java/com/favorezapp/goodfood/allrecipes/presentation/AllFoodRecipesEvent.kt
+++
@@ -1,6 +0,0 @@
-package com.favorezapp.goodfood.allrecipes.presentation
-
-sealed class AllFoodRecipesEvent {
-    object RequestInitialRecipes: AllFoodRecipesEvent()
-    object RequestMoreRecipes: AllFoodRecipesEvent()
-}
