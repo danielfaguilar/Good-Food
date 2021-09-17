32299e38673cd40da1adcc9b4683414847ce4c88
Daniel Flores <daniel.flores.fullstack@gmail.com>
2021-09-16 19:00:16 -0500

    New mappers for the food recipe main mapper

--- app/src/main/java/com/favorezapp/goodfood/common/data/api/model/mappers/ApiFoodRecipeMapper.kt
+++ app/src/main/java/com/favorezapp/goodfood/common/data/api/model/mappers/ApiFoodRecipeMapper.kt
@@ -1,17 +1,25 @@
 package com.favorezapp.goodfood.common.data.api.model.mappers
 
+import com.favorezapp.goodfood.common.data.api.model.ApiAnalyzedInstruction
 import com.favorezapp.goodfood.common.data.api.model.ApiExtendedIngredient
 import com.favorezapp.goodfood.common.data.api.model.ApiFoodRecipe
+import com.favorezapp.goodfood.common.domain.model.foodrecipe.AnalyzedInstructions
 import com.favorezapp.goodfood.common.domain.model.foodrecipe.FoodRecipe
+import java.lang.NullPointerException
 import javax.inject.Inject
 
 class ApiFoodRecipeMapper @Inject constructor(
-    private val apiExtendedIngredientsMapper: ApiExtendedIngredientMapper
+    private val apiExtendedIngredientsMapper: ApiExtendedIngredientMapper,
+    private val analyzedInstructionMapper: ApiAnalyzedInstructionMapper
 ): ApiMapper<ApiFoodRecipe, FoodRecipe> {
     override fun mapToDomain(entity: ApiFoodRecipe) =
         FoodRecipe(
             entity.id,
             entity.aggregateLikes ?: 0,
+            parseAnalyzedInstructions(
+                entity.title ?: throw NullPointerException("Recipe title cannot be null")
+                , entity.analyzedInstructions
+            ),
             entity.author.orEmpty(),
             entity.cheap ?: false,
             entity.cuisines.orEmpty(),
@@ -29,6 +37,19 @@ class ApiFoodRecipeMapper @Inject constructor(
             entity.vegetarian ?: false,
             entity.veryHealthy ?: false
         )
+
+    private fun parseAnalyzedInstructions(
+        title: String,
+        analyzedInstructions: List<ApiAnalyzedInstruction>?
+    ): AnalyzedInstructions {
+        return AnalyzedInstructions(
+            analyzedInstructions.orEmpty().map {
+                analyzedInstructionMapper
+                    .mapToDomain(it)
+                    .copy(recipeTitle = title)
+            }
+        )
+    }
 
     private fun parseExtendedIngredients(apiExtendedIngredients: List<ApiExtendedIngredient>) =
         apiExtendedIngredients.map {
